cdb721c0b228bf9cc71fa8e7377f17c14324f64c
PrettyMathedimosa <mathedimosapritty@gmail.com>
2024-02-19 02:29:29 -0800

    12:29

--- introductionphp/folder/test.php
+++ introductionphp/folder/test.php
@@ -1,6 +1,17 @@
 <?php
 $con = mysqli_connect("localhost","root","","image");
 
+if(isset($_POST['upload'])){
+    $file = $_FILES['image']['name']
+
+    $query = "INSERT INTO upload(image) VALUES('$file')";
+    $RES = mysqli_query($con,$query);
+
+    if($res){
+        move_uploaded_file($_FILES['image']['tmp_name'], "$file");
+    }
+}
+
 
 
 
@@ -33,6 +44,22 @@ $con = mysqli_connect("localhost","root","","image");
                     </div>
                     <div class="col-md-6">
                         <h4 class="text-center">DISPLAY IMAGE</h4>
+
+                        <?php
+                            $sel = "SELECT * FROM upload";
+                            $que = mysqli_query($con,$sel);
+
+                            $output ="";
+
+                            if(mysqli_num_rows($que) < 1){
+                                $output = "h5 class='tetx-center'>No image uploaded</h5>";
+                            }
+
+                            while($row = mysqli_fetch_array($que)){
+                                $output = "<img src='" $row['image'] "'class='my-3'
+                                     style='width:400px;height:400px;'>"
+                            }
+                        ?>
 
                     </div>
 
