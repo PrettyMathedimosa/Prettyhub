3c5591e4016664a4dfd3ae5e0f9da0d44dd79e5c
PrettyMathedimosa <mathedimosapritty@gmail.com>
2024-02-20 00:21:53 -0800

    10:21

--- introductionphp/tables/tables.php
+++ introductionphp/tables/tables.php
@@ -12,4 +12,15 @@ $conn = new mysqli($servername,$username,$password,$dbname );
 if($conn->connect_error){
     die("connection failed: ".$conn->connect_error);
 }
+
+//sql to create table
+CREATE TABLE MyGuests(
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY ,
+    firstname VARCHAR(30) NOT NULL,
+    lastname  VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    reg_date TIMESTAMP DEFAULT
+    CURRENT_TIMESTAMP ON UPDATE
+    CURRENT_TIMESTAMP
+)
 ?>
