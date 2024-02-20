ccc4de5c2d392f6a8938825a10c31c76f8e547d8
PrettyMathedimosa <mathedimosapritty@gmail.com>
2024-02-19 23:15:50 -0800

    09:15

---
+++ introductionphp/tables/tables.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+//create connection
+$conn = new mysqli($servername,$username,$password,$dbname );
+
+//check connection
+if($conn->connect_error){
+    die("connection failed: ".$conn->connect_error);
+}
+?>
