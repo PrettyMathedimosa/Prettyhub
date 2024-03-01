67622aa65788149a731bdb37793f99522f34f172
PrettyMathedimosa <mathedimosapritty@gmail.com>
2024-03-01 04:47:51 -0800

    14:47

--- bootstrap/mydatabse.php
+++ bootstrap/mydatabse.php
@@ -8,5 +8,17 @@ $password = " ";
 //connecting to sql
 $conn = new mysqli($servername,$username,$password);
 
+//checking if connected
+if($conn->connect_error)
+{
+    die("not connected: " .$conn->connect_error);
+}
+echo "connected";
 
+//creating a database
+$sql = "CREATE DATABASE    ";
+
+if($conn->query($sql) === TRUE){
+    echo "database created successfully";
+}
 ?>
