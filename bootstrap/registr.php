5a9b9b920302908919919d29f386eb3b622f677f
PrettyMathedimosa <mathedimosapritty@gmail.com>
2024-03-01 00:39:23 -0800

    10:39

---
+++ bootstrap/registr.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+   <html lang="en">
+    <meta charset="utf-8">
+    <head>
+         <title> Bootstrap </title>
+         <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div>
+            <?php
+
+            ?>
+
+        </div>
+
+    </body>
+    </html> 
