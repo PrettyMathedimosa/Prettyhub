4d6741b46f6835d23fb2d8c7ceced6f349dea2aa
PrettyMathedimosa <mathedimosapritty@gmail.com>
2024-03-06 01:27:09 -0800

    11:26

--- bootstrap/sign.php
+++ bootstrap/sign.php
@@ -2,10 +2,17 @@
    <html lang="en">
     <meta charset="utf-8">
     <head>
-         <title> About Us </title>
+         <title> sign in </title>
          <link rel="stylesheet" href="style.css">
     </head>
     <body>
+        <nav class="navbar bg-dark navbar-dark justify-content-center">
+            <div class ="container">
+                <a class="navbar-brand" href="#"> sign in page </a>
+                <ul class="navbar-nav flex-row justify-content-between" style="width:600px;">
+
+
+
 
     </body>
     </html>
