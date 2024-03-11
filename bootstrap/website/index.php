deeb8b4e71e2fcbc5eaecba11ddceaed3aca2112
PrettyMathedimosa <mathedimosapritty@gmail.com>
2024-03-11 01:27:59 -0700

    10:27

--- bootstrap/website/index.php
+++ bootstrap/website/index.php
@@ -4,17 +4,31 @@
         <title> multi-user role-based-login-system </title>
     </head>
 <body>
-    <div class="container">
-        <form>
+    <div class="container d-flex justify-content-center
+         align-items-center"
+         style="min-height:100vh">
+        <form class="border shadow p-3 rounded"
+               style="width:450px;">
+               <h1 class="text-center p-3">LOGIN</h1>
             <div class="mb-3">
                 <label for="username"
                        class="form-label">Username</label>
                 <input type="text"
                        class="form-control"
+                       name="username"
                        id="username">
             </div>
+            <div class="mb-3">
+                <label for="password"
+                       class="form-label">Password</label>
+                <input type="text
+                       class="form-control"
+                       name="password"
+                       id="password">
 
             </div>
+
+           
         </form>
 
     </div>
