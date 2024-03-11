0f05f897815f1cbbcd29e4fb4e3ee01156ea9e4f
PrettyMathedimosa <mathedimosapritty@gmail.com>
2024-03-11 03:10:04 -0700

    12:09

---
+++ bootstrap/website/check-login.php
@@ -0,0 +1,6 @@
+<?php
+if(isset($_POST['username']) && isset($_POST['password']) && 
+   isset($_POST['role'])) {
+    
+   }
+?>
--- bootstrap/website/index.php
+++ bootstrap/website/index.php
@@ -8,6 +8,8 @@
          align-items-center"
          style="min-height:100vh">
         <form class="border shadow p-3 rounded"
+            action="php/check-login.php"
+            method="post"
                style="width:450px;">
                <h1 class="text-center p-3">LOGIN</h1>
             <div class="mb-3">
@@ -21,22 +23,24 @@
             <div class="mb-3">
                 <label for="password"
                        class="form-label">Password</label>
-                <input type="text
+                <input type="text"
                        class="form-control"
                        name="password"
                        id="password">
             </div>
-            <div class="mb-3">
+            <div class="mb-1">
                 <label class="form-label">Select User Type:</label>
             </div>
 
             <select class="form-select mb-3"
+                    name="role"
                     aria-label="Default select example">
-                    <option selected>Open this select menu</option>
-                    <option value="1">One</option>
+                    <option selected value="user">User</option>
+                    <option value="1">Admin</option>
 
             </select>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit"
+                    class="btn btn-primary">Submit</button>
 
            
         </form>
