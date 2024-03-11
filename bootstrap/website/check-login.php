922fd6845300e1d51ceb8d7ed8d9157a5d6f6057
PrettyMathedimosa <mathedimosapritty@gmail.com>
2024-03-11 05:07:51 -0700

    14:07

--- bootstrap/website/check-login.php
+++ bootstrap/website/check-login.php
@@ -1,6 +1,20 @@
 <?php
 if(isset($_POST['username']) && isset($_POST['password']) && 
    isset($_POST['role'])) {
+
+      function test_input($data){
+         $data = trim($data);
+         $data = stripcslashes($data);
+         $data = htmlspecialchars($data);
+         return $data;
+      }
+
+      $username = test_input($_POST['username']);
+      $password = test_input($_POST['password']);
+      $role = test_input($_POST['role']);
+      
     
-   }else
+   }else{
+      header("Location:../index.php");
+   }
 ?>
