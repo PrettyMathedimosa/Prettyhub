976d0c74103e582d9eb3c952b98ad7bc92617e69
PrettyMathedimosa <mathedimosapritty@gmail.com>
2024-03-11 01:13:23 -0700

    10:18

---
+++ bootstrap/website/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> multi-user role-based-login-system </title>
+    </head>
+<body>
+    <div class="container">
+        <form>
+            <div class="mb-3">
+                <label for="username"
+                       class="form-label">Username</label>
+                <input type="text"
+                       class="form-control"
+                       id="username">
+            </div>
+
+            </div>
+        </form>
+
+    </div>
+</body>
+</html>
