5681bd174be57b8218315ec2751b591dd52b37af
PrettyMathedimosa <mathedimosapritty@gmail.com>
2024-03-12 02:42:46 -0700

    11:42

--- bootstrap/website/index.php
+++ bootstrap/website/index.php
@@ -12,12 +12,12 @@
             method="post"
                style="width:450px;">
                <h1 class="text-center p-3">LOGIN</h1>
-               <?php if(isset($_GET['error'])){
+               <?php if(isset($_GET['error'])){?>
 
-               }?>
                <div class="alert alert-danger" role="alert">
-                   A simple danger alert-check it out
+                  <?=$_GET['error']?>
                </div>
+               <?php}?>
             <div class="mb-3">
                 <label for="username"
                        class="form-label">Username</label>
