2b1c381ac4ab3636d471aebfa63870b08a4f8706
PrettyMathedimosa <mathedimosapritty@gmail.com>
2024-03-14 04:03:52 -0700

    13:03

--- bootstrap/website/home.php
+++ bootstrap/website/home.php
@@ -10,6 +10,17 @@
          align-items-center"
          style="min-height:100vh">
 
+         <?php if($_SESSION['role'] == 'admin'){
+           //for admin
+           <div class ="card" style="width:15rem;">
+           <div class ="card-body">
+           <h2 class="card-title"> Card title</h2>
+           <p class="card-text">some card text and make up the bulk of the card content</p>
+           </div>
+           </div>
+         }
+         ?>
+
          <h1> Hello World </h1>
     </div>
 
