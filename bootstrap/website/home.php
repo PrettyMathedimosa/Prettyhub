e5d72c6a86fbfd561f3089c9fbb596a2e35d4233
PrettyMathedimosa <mathedimosapritty@gmail.com>
2024-03-14 02:44:29 -0700

    11:44

---
+++ bootstrap/website/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+   <html lang="en">
+    <meta charset="utf-8">
+    <head>
+         <title> Home page   </title>
+         <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    <div class="container d-flex justify-content-center
+         align-items-center"
+         style="min-height:100vh">
+
+         <h1> Hello World </h1>
+    </div>
+
+</body>
+</html>
