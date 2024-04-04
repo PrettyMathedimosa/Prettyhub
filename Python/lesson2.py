0b68e378cfc7cb565c6e3cf9ae6c1136db3ee7d4
PrettyMathedimosa <mathedimosapritty@gmail.com>
2024-04-04 03:40:23 -0700

    12:40

--- Python/lesson2.py
+++ Python/lesson2.py
@@ -9,3 +9,9 @@ def main():
     root = Tk()
     app = windows1(root)
     root.mainloop()
+
+class windows1:
+    def _init_(self,master):
+        self.master = master
+        self.master.title("Phatmacy Management System")
+        self.master.geometry('1350X750+0+0')
