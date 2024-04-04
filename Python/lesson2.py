4663ff4bbbbbab309da89577680acf1276ea0254
PrettyMathedimosa <mathedimosapritty@gmail.com>
2024-04-04 06:01:43 -0700

    15:01

--- Python/lesson2.py
+++ Python/lesson2.py
@@ -18,6 +18,15 @@ class windows1:
         self.frame = Frame(self.master)
         self.frame.pack()
 
-        self.LabelTitle = Label(self.frame,text = "Pharmacy Management Sytem",font = ("arial",40,"bold")),
+        self.LabelTitle = Label(self.frame,text = "Pharmacy Management Sytem",font = ("arial",40,"bold"),
+            bd = 10,relief = "sunken")
+        self.LabelTitle.grid(row = 0,column = 0,columnspan = 2,pady = 20)
 
+class windows1:
+    def _init_(self,master):
+        self.master = master
+        self.master.title("Pharmacy Management System")
+        self.master.geometry('1350X750+0+0')
+        self.frame = Frame(self.master)
+        self.frame.pack()
 
