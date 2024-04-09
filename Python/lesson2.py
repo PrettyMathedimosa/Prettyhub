ecfdf2eb5fd93cd5d8c1a2473a09d5d0aa6d9e8c
PrettyMathedimosa <mathedimosapritty@gmail.com>
2024-04-09 05:55:53 -0700

    14:55

--- Python/lesson2.py
+++ Python/lesson2.py
@@ -80,19 +80,19 @@ class windows5:
         self.Loginframe3 = Frame(self.frame,width = 1000,height = 200,bd = 10,relief = "groove")
         self.Loginframe3.grid(row = 6,column = 0, pady = 5)
 
-        self.button_reg = Button(self.Loginframe3,text = "Patient registration window",font = ("arial",15,"bold"),
+        self.button_reg = Button(self.Loginframe3,text = "Patient registration window",state = DISABLED,font = ("arial",15,"bold"),
                                  command = self.Registration_window)
         self.button_reg.grid(row = 1, column = 0, padx = 10, pady = 10)
 
-        self.button_hosp = Button(self.Loginframe3,text = "Patient registration window",font = ("arial",15,"bold"),
+        self.button_hosp = Button(self.Loginframe3,text = "Patient registration window",state = DISABLED,font = ("arial",15,"bold"),
                                  command = self.Hospital_window)
         self.button_hosp.grid(row = 2, column = 1, padx = 10, pady = 10)
 
-        self.button_Dr_appt = Button(self.Loginframe3,text = "Patient registration window",font = ("arial",15,"bold"),
+        self.button_Dr_appt = Button(self.Loginframe3,text = "Patient registration window",state = DISABLED,font = ("arial",15,"bold"),
                                  command = self.Dr_Apoint_window)
         self.button_Dr_appt.grid(row = 1, column = 0, padx = 10, pady = 10)
 
-        self.button_med_stock = Button(self.Loginframe3,text = "Patient registration window",font = ("arial",15,"bold"),
+        self.button_med_stock = Button(self.Loginframe3,text = "Patient registration window",state = DISABLED,font = ("arial",15,"bold"),
                                  command = self.Medicine_stock)
         self.button_med_stock.grid(row = 1, column = 1, padx = 10, pady = 10)
 
@@ -113,11 +113,21 @@ class windows5:
         self.button_login = Button(self.Loginframe2,text = "Login",width = 20,font = ("arial",18,"bold"))
         self.button_login.grid(row = 0,column = 0,padx = 10,pady = 10)
 
-        self.button_Reset = Button(self.Loginframe2,text = "Login",width = 20,font = ("arial",18,"bold"))
-        self.button_Reset.grid(row = 0,column = 0,padx = 10,pady = 10)
+        self.button_Reset = Button(self.Loginframe2,text = "Reset",width = 20,font = ("arial",18,"bold"))
+        self.button_Reset.grid(row = 0,column = 3,padx = 10,pady = 10)
 
-        self.button_Exit = Button(self.Loginframe2,text = "Login",width = 20,font = ("arial",18,"bold"))
-        self.button_Exit.grid(row = 0,column = 0,padx = 10,pady = 10)
+        self.button_Exit = Button(self.Loginframe2,text = "Exit",width = 20,font = ("arial",18,"bold"))
+        self.button_Exit.grid(row = 0,column = 6,padx = 10,pady = 10)
+
+    def login_system(self):
+        user = self.Username.get()
+        pswd = self.Password.get()
+
+        if(user == str("admin") and (pswd == str("admin"))):
+            self.button_reg.config(state = NORMAL)
+            self.button_Hosp.config(state = NORMAL)
+            self.button_Dr_appt.config(state = NORMAL)
+            self.button_med_stock.config(state = NORMAL)
 
     #define the windows
     def Registration_window(self):
